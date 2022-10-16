0f698822f3cb7d3963a82093b4294eba1abfd2e5
Nina Rybak <rybaknin@gmail.com>
2022-10-16 13:32:56 +0300

    task-1 add argv

--- cli.php
+++ cli.php
@@ -8,10 +8,15 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 $faker = Faker\Factory::create();
 
-$user = new User(1, $faker->firstName, $faker->lastName);
-$post = new Post(1, $user, $faker->jobTitle, $faker->text);
-$comment = new Comment(1, $user, $post, $faker->text);
+$user = new User($faker->randomDigitNotNull, $faker->firstName, $faker->lastName);
+$post = new Post($faker->randomDigitNotNull, $user, $faker->jobTitle, $faker->text);
+$comment = new Comment($faker->randomDigitNotNull, $user, $post, $faker->text);
 
-print $user . PHP_EOL;
-print $post . PHP_EOL;
-print $comment . PHP_EOL;
+$route = $argv[1] ?? null;
+
+echo match ($route) {
+    "user" => $user,
+    "post" => $post,
+    "comment" => $comment,
+    default => "error argument, should be one of them: user, post, comment",
+};
